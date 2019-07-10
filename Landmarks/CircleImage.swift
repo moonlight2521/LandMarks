5dd78b2c9a396fbfb6c1d5e665954f37d519b535
Zun Lin <linzl@mymail.vcu.edu>
2019-07-10 03:17:47 -0400

    Add files via upload

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,26 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Zun Lin on 7/9/19.
+//  Copyright © 2019 Zun Lin. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage : View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 5))
+            .shadow(radius: 10)
+    }
+}
+
+#if DEBUG
+struct CircleImage_Previews : PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
+#endif
---
+++ Landmarks/MapView.swift
@@ -0,0 +1,30 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Zun Lin on 7/9/19.
+//  Copyright © 2019 Zun Lin. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView : UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView{
+        MKMapView(frame: .zero)
+    }
+    func updateUIView(_ view: MKMapView, context: Context){
+        let coordinate = CLLocationCoordinate2DMake(34.011286, -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        view.setRegion(region, animated: true)
+    }
+}
+
+#if DEBUG
+struct MapView_Previews : PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
+#endif
